423009d90c7d682db0281ec5ed7037b316e41f9a
danikdesign <danik.newmail@gmail.com>
2022-11-14 21:38:56 +0200

    Added view of products on main page. Add logo image

--- app.rb
+++ app.rb
@@ -6,11 +6,14 @@ require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 
 class Product < ActiveRecord::Base
+end
 
+before do
+  @products = Product.order 'is_best_offer DESC'
 end
 
 get '/' do
-	erb :index
+  erb :index
 end
 
 get '/about' do
