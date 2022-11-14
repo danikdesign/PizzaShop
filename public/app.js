3df44eed24d791ef58b3d958f9ce75d595e576f3
danikdesign <danik.newmail@gmail.com>
2022-11-14 22:47:22 +0200

    Update 'add_to_cart' js function

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
 function add_to_cart(id)
 {
-    alert('You choosed pizza with id: ' + id);
+    var key = 'product_' + id;
+    
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x)
 }
