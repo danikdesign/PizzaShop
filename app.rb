c00ec52eea8408e30577564ebd2229754970f69b
danikdesign <danik.newmail@gmail.com>
2022-11-17 20:19:12 +0200

    Parse orders input

--- app.rb
+++ app.rb
@@ -28,7 +28,11 @@ get '/cart' do
 end
 
 post '/cart' do
-  erb :cart
+  orders_input = params[:orders_input]
+  @order = parse_orders_input orders_input
+
+
+  erb "Hello it's your order: #{@order.inspect}"
 end
 
 post '/order' do
@@ -40,4 +44,23 @@ post '/order' do
     @error = @order.errors.full_messages.first
     erb :cart
   end
+end
+
+def parse_orders_input orders_input
+  s1 = orders_input.split(',')
+  arr = []
+
+  s1.each do |x|
+    s2 = x.split('=')
+    s3 = s2[0].split('_')
+
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
 end
